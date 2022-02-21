1f2977b2e1b14f5d4308655dc9b7f2b686bba978
Muhammet Cin <muhammetacin@gmail.com>
2022-02-21 11:03:50 +0100

    Reset experimental.php file

--- experimental.php
+++ experimental.php
@@ -1,73 +1,85 @@
 <?php
-//
-//function orderPizza($pizzaType, $client) {
-//    echo "Creating new order...   <br><br>";
-//
-//    $type = $pizzaType;
-//    $pizzaPrice = calculateCost($type);
-//    $clientAddress = "unknown";
-//
-//    if($client === "koen")
-//    {
-//        $clientAddress = "a yacht in Antwerp";
-//    }
-//    else if ($client === "manuele")
-//    {
-//        $clientAddress = "somewhere in Belgium";
-//    }
-//    else if ($client === "students") {
-//        $clientAddress = "BeCode office";
-//    }
-//
-//    $printAddress = "The address is: $clientAddress.";
-//    $printPrice = "The bill is € $pizzaPrice.";
-//
-//    $printOrder = "A " . $type . " pizza should be sent to " . $client . "."
-//        . "<br>"
-//        . $printAddress
-//        . "<br>"
-//        . $printPrice
-//        . "<br>";
-//
-//    echo $printOrder;
-//    echo "Order finished.<br><br>";
-//}
-//
-//function calculateCost($pizzaType)
-//{
-//    $costPrice = 0;
-//
-//    if ($pizzaType === "marguerita") {
-//        $costPrice = 5;
-//    }
-//    else if ($pizzaType === "golden")
-//    {
-//        $costPrice = 100;
-//    }
-//    else if ($pizzaType === "calzone")
-//    {
-//        $costPrice = 10;
-//    }
-//    else if ($pizzaType === "hawaii") {
-//        throw new Exception("There is no pizza type 'Hawaii'.");
-//    }
-//
-//    return $costPrice;
-//}
-//
-//function orderPizzaForAll()
-//{
-//    orderPizza('calzone', 'koen');
-//    orderPizza('marguerita', 'manuele');
-//    orderPizza('golden', 'students');
-//}
-//
-//function makeEveryoneHappy($orderPizzaForClass) {
-//    if ($orderPizzaForClass) {
-//        orderPizzaForAll();
-//        return true;
-//    }
-//    return false;
-//}
-//
-//makeEveryoneHappy(true);
+
+// fw = for who
+function ordr_pz($pizzatype, $fw) {
+
+$type = $pizzatype;
+echo 'Creating new order... <br>';
+$toPrint = 'A ';
+ $toPrint .= $pizzatype;
+$p = calc_cts($type);
+
+    $address = 'unknown';
+    if($fw == 'koen')
+    {
+        $address = 'a yacht in Antwerp';
+    } elseif ($fw == 'manuele')
+    {
+        $address = 'somewhere in Belgium';
+    } elseif ($fw == 'students') {
+        $address = 'BeCode office';
+    }
+
+        $toPrint .=   ' pizza should be sent to ' . $fw . ". <br>The address: {$address}.";
+    echo $toPrint; echo '<br>';
+    echo'The bill is €'.$p.'.<br>';
+
+
+
+
+    echo "Order finished.<br><br>";
+}
+
+    function total_price($p) {
+            return $p;
+    }
+
+        function test($p_type) {
+            echo "Test: type is {$p_type}. <br>";
+        }
+
+function calc_cts($p_type)
+{
+    $cst = 'unknown';
+
+    if ($p_type == 'marguerita') {
+        $cst = 5;
+    }
+    else
+        {
+        if ($p_type == 'golden')
+        {
+            $cst = 100;
+        }
+
+            if ($p_type == 'calzone')
+            {
+                $cst = 10;
+            }
+
+        if ($p_type == 'hawaii') {
+            throw new Exception('Computer says no');
+        }
+    }
+
+    return $cst;
+}
+
+            function ordr_piz_all()
+            {
+            $test= 0;
+            ordr_pz('calzone', 'koen');
+            ordr_pz('marguerita', 'manuele');
+
+            ordr_pz('golden', 'students');
+            }
+
+function make_Allhappy($do_it) {
+if ($do_it) {
+        ordr_piz_all();
+    } else {
+        // Should not do anything when false
+    }
+}
+
+make_Allhappy(true);
